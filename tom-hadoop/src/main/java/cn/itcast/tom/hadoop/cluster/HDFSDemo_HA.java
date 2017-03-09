a30f6ab3c0ac52d84fa640728d6ee03490c7010a
zjw641220480 <1967580689@qq.com>
2017-03-09 21:37:34 +0800

    整理hadoop,为集群准备

---
+++ tom-hadoop/src/main/java/cn/itcast/tom/hadoop/cluster/HDFSDemo_HA.java
@@ -0,0 +1,11 @@
+package cn.itcast.tom.hadoop.cluster;
+/**
+ *
+ * <p>Title:HDFSDemo_HA.java</p>
+ * <p>Description:</p>
+ * @author TOM
+ * @date 2017年3月9日下午9:32:05
+ */
+public class HDFSDemo_HA {
+
+}
